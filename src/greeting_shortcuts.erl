db53179b814ceb3fbcc7740e9898de705df87abb
Ralf Th. Pietsch <ratopi@abwesend.de>
2017-12-02 19:49:52 +0100

    Storing and reading cookies

--- src/greeting_shortcuts.erl
+++ src/greeting_shortcuts.erl
@@ -12,7 +12,18 @@
 %% API
 -compile(export_all).
 
+
 render_ok(Req, TemplateModule, Params) ->
+	render_ok(Req, [], TemplateModule, Params).
+
+
+render_ok(Req, Headers, TemplateModule, Params) ->
 	{ok, Output} = TemplateModule:render(Params),
-	% Here we use mochiweb_request:ok/1 to render a reponse
-	Req:ok({"text/html", Output}).
+	Req:ok({"text/html", Headers, Output}).
+
+
+get_cookie_value(Req, Key, Default) ->
+	case Req:get_cookie_value(Key) of
+		undefined -> Default;
+		Value -> Value
+	end.
